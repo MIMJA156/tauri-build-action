ee40b8746e21ada25f7dcb0694057842a49efb46
MIMJA156 <75694661+MIMJA156@users.noreply.github.com>
2024-03-02 23:09:37 -0700

    new start
    the generated template was to much so I started from scratch

--- README.md
+++
@@ -1,4 +0,0 @@
-# Tauri Build Action
-
-This action is so you can build and deploy updates all from github actions.
-(yay, this is mostly for me but if people like it I will make it even better!)
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,1 @@
-/**
- * The entrypoint for the action.
- */
-import { run } from './main'
-
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
-run()
+console.log("hello world!");
